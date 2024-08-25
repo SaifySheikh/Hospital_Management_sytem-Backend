fd6bcf880dc1fd6b210cafd1e0d06b1a28bea5bf
SaifySheikh <sheikhmm_4@rknec.edu>
2024-08-25 17:15:32 +0530

    commit

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
